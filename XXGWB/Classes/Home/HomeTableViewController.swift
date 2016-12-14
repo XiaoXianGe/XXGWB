8f286cf2007f3b5212cc24a269124d3989f908f7
LeeSamYear <258189072@qq.com>
2016-12-14 17:54:53 +0800

    首页nav

--- XXGWB/Classes/Home/HomeTableViewController.swift
+++ XXGWB/Classes/Home/HomeTableViewController.swift
@@ -20,22 +20,24 @@ class HomeTableViewController: BaseTableViewController {
         }
         
         //添加导航栏按钮-通过添加Button
+        setUpNavigation()
+    }
+    
+    private func setUpNavigation(){
         navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "navigationbar_friendattention", target: self, action:#selector(HomeTableViewController.leftBtnClick))
         navigationItem.rightBarButtonItem = UIBarButtonItem(imageName: "navigationbar_pop", target: self, action: #selector(HomeTableViewController.rightBtnClick))
         
         //设置导航栏的标题
         let titleBtn = TitleButton()
         titleBtn.setTitle("小贤哥", for: UIControlState.normal)
-        titleBtn.setImage(UIImage(named:"navigationbar_arrow_down"), for: UIControlState.normal)
-        titleBtn.addTarget(self, action: #selector(HomeTableViewController.titleBtnClick(titleBtn:)), for: UIControlEvents.touchUpInside)
-        titleBtn.sizeToFit()
-        navigationItem.titleView = titleBtn
         
-       
-
+        titleBtn.addTarget(self, action: #selector(HomeTableViewController.titleBtnClick(titleBtn:)), for: UIControlEvents.touchUpInside)
+        
+        navigationItem.titleView = titleBtn
     }
     
     @objc private func titleBtnClick(titleBtn:TitleButton){
+        XGLog(message: "titleBtnClick")
         titleBtn.isSelected = !titleBtn.isSelected
     }
     @objc private func leftBtnClick(){
--- XXGWB/Classes/Tools/TitleButton.swift
+++ XXGWB/Classes/Tools/TitleButton.swift
@@ -10,19 +10,38 @@ import UIKit
 
 class TitleButton: UIButton {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpUI()
+    }
+
+    func setUpUI() {
+        setImage(UIImage(named:"navigationbar_arrow_down"), for: UIControlState.normal)
+        setImage(UIImage(named:"navigationbar_arrow_up"), for: UIControlState.selected)
+        sizeToFit()
+    }
+    
     
     override func setTitle(_ title: String?, for state: UIControlState) {
+        
+        super.setTitle((title ?? "") + "  ", for: state)
         
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        setTitleColor(UIColor.black, for: UIControlState.normal)
+        setTitleColor(UIColor.darkGray, for: UIControlState.normal)
         
         titleLabel?.frame.origin.x = 0
         imageView?.frame.origin.x = (titleLabel?.frame.width)!
-
+        
     }
-
+    
 }
